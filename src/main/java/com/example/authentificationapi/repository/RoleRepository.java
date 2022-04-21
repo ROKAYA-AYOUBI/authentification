412f5327689cfe5192e42f4a1f8d109c018e187f
ROKAYA-AYOUBI <rokiaayoubi@gmail.com>
2022-04-21 13:46:21 +0000

    message

---
+++ src/main/java/com/example/authentificationapi/repository/RoleRepository.java
@@ -0,0 +1,15 @@
+package com.example.authentificationapi.repository;
+
+import java.util.Optional;
+import com.example.authentificationapi.models.Role;
+import com.example.authentificationapi.models.ERole;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
+}
---
+++ src/test/java/com/example/authentificationapi/AuthentificationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authentificationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthentificationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
